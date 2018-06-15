a7044a81e89dbc950d342b59389fc10152655231
Dimitri Kleyn <dimitri.kleyn@fv.nrw.de>
2018-06-15 14:22:58 +0200

    Run Fatjar directly.

--- DasViewer.bat
+++ DasViewer.bat
@@ -1,8 +1,1 @@
-@echo off
-set CLASSPATH=%~dp0out\production\classes;%~dp0out\production\resources
-@rem for %%i in ("%~dp0target\dependency\*.jar") do call :addcp %%i
-java -Dfile.encoding=ISO-8859-15 dem2k.DasViewer %*
-goto ende
-:addcp
-set CLASSPATH=%1;%CLASSPATH%
-:ende
+java -Dfile.encoding=ISO-8859-15 -jar %~DP0build\libs\DasViewer.jar %*
